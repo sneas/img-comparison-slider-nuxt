3cdef1537f91525ade2e0e3a46e38f696dcddd44
Dima Snisarenko <snisarenkodima@gmail.com>
2022-07-15 09:38:25 +0200

    feat: img-comparison-slider usage example

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <client-only placeholder="loading...">
+    <ImgComparisonSlider>
+      <!-- eslint-disable -->
+      <img
+          slot="first"
+          style="width: 100%"
+          src="https://img-comparison-slider.sneas.io/demo/images/before.webp"
+      />
+      <img
+          slot="second"
+          style="width: 100%"
+          src="https://img-comparison-slider.sneas.io/demo/images/after.webp"
+      />
+      <!-- eslint-enable -->
+    </ImgComparisonSlider>
+  </client-only>
+</template>
+
+<script>
+import { defineComponent, defineAsyncComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ImgComparisonSlider: defineAsyncComponent(() => import('@img-comparison-slider/vue').then(({ImgComparisonSlider}) => ImgComparisonSlider
+    ))
+  },
+})
+</script>
